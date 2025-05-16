69fbbe3f4002f8bd15c14c0b00f36821a158a1e2
oliverneut <oliver.neut@gmail.com>
2025-05-16 10:55:31 +0200

    Made qpp faster

--- src/qpp.py
+++ src/qpp.py
@@ -11,6 +11,7 @@ from torch.utils.data import DataLoader
 from data_loaders import EmbeddingDataset
 from utils.data_utils import DatasetConfig
 from tqdm import tqdm
+from collections import defaultdict
 import logging
 
 logger = logging.getLogger(__name__)
@@ -19,6 +20,8 @@ def uncertainty_score(cov):
     return torch.linalg.vector_norm(cov, ord=2, dim=2)
 
 def main(args, run_id: str, data_cfg: DatasetConfig):
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO)
+
     model_dir = f"output/models/{run_id}"
     psg_embs_path = f"{model_dir}/psg_embs.pt"
     psg_ids_path = f"{model_dir}/psg_ids.pt"
@@ -38,13 +41,16 @@ def main(args, run_id: str, data_cfg: DatasetConfig):
     uncertainty_scores = []
     ndcg_scores = []
     mrr_scores = []
-    
+    run = {}
+    qpp_scores = defaultdict(dict)
+
     with torch.no_grad():
         for qry_embs, qry_ids in tqdm(qry_dataloader, desc="Computing QPP scores"):
             mean, cov = qry_embs[:,0,:], qry_embs[:,1:,:]
-            run = {}
-            uncertainty = uncertainty_score(cov)
-            uncertainty_scores += uncertainty.squeeze().tolist()
+
+            uncertainty_scores += uncertainty_score(cov).squeeze().tolist()
+            for qry_id, uncertainty in zip(qry_ids, uncertainty_scores):
+                qpp_scores[qry_id]['uncertainty'] = uncertainty
 
             scores, indices = index.search(mean, k=10)
 
@@ -55,12 +61,16 @@ def main(args, run_id: str, data_cfg: DatasetConfig):
                     for score, psg_id in zip(query_scores, query_indices):
                         run[qry_id][psg_id] = float(score)
 
-            evaluator = RelevanceEvaluator(qrels, {"ndcg", "recip_rank"})
-            results = evaluator.evaluate(run)
+        evaluator = RelevanceEvaluator(qrels, {"ndcg", "recip_rank"})
+        results = evaluator.evaluate(run)
 
-            for _, metrics in results.items():
-                 ndcg_scores.append(metrics['ndcg'])
-                 mrr_scores.append(metrics['recip_rank'])
+        for qry_id, metrics in results.items():
+            qpp_scores[qry_id]['ndcg'] = metrics['ndcg']
+            qpp_scores[qry_id]['mrr'] = metrics['recip_rank']
+
+    uncertainty_scores = [qpp_scores[qry_id]['uncertainty'] for qry_id in qpp_scores]
+    ndcg_scores = [qpp_scores[qry_id]['ndcg'] for qry_id in qpp_scores]
+    mrr_scores = [qpp_scores[qry_id]['mrr'] for qry_id in qpp_scores]
 
     ndcg_corr =  pearsonr(uncertainty_scores, ndcg_scores)
     mrr_corr = pearsonr(uncertainty_scores, mrr_scores)
