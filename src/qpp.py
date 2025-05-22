576a4faa301563b6c4de4278acf1f3d8720ef67f
oliverneut <oliver.neut@gmail.com>
2025-05-22 23:32:24 +0200

    Added uncertainty based on weights covariance to qpp

--- src/qpp.py
+++ src/qpp.py
@@ -5,6 +5,7 @@ import wandb
 from omegaconf import OmegaConf
 from types import SimpleNamespace
 from indexing import FaissIndex
+from utils.model_utils import vbll_model_factory
 from pytrec_eval import RelevanceEvaluator
 from data_loaders import get_qrels
 from torch.utils.data import DataLoader
@@ -19,10 +20,11 @@ logger = logging.getLogger(__name__)
 def uncertainty_score(cov):
     return torch.linalg.vector_norm(cov, ord=2, dim=1)
 
-def main(run_id: str, data_cfg: DatasetConfig):
+def main(args: SimpleNamespace, run_id: str, data_cfg: DatasetConfig):
     logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO)
     logger.info(f"Run ID: {run_id}")
     logger.info(f"Dataset id: {data_cfg.dataset_id}")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model_dir = f"output/models/{run_id}"
     psg_embs_path = f"{model_dir}/psg_embs.pt"
@@ -32,6 +34,14 @@ def main(run_id: str, data_cfg: DatasetConfig):
 
     psg_embs = torch.load(psg_embs_path)
     psg_ids = torch.load(psg_ids_path)
+
+    model_path = f"{model_dir}/model.pt"
+
+    _, model = vbll_model_factory(args.model_name, 1, args.parameterization, args.prior_scale, args.wishart_scale, device)
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
+
+    noise = model.vbll_layer.noise()
 
     qry_dataset = EmbeddingDataset(qry_embs_path, qry_ids_path)
     qry_dataloader = DataLoader(qry_dataset, batch_size=16, shuffle=False)
@@ -50,6 +60,7 @@ def main(run_id: str, data_cfg: DatasetConfig):
         for qry_embs, qry_ids in tqdm(qry_dataloader, desc="Computing QPP scores"):
             mean, cov = qry_embs[:,0,:], qry_embs[:,1,:]
 
+            cov = cov - noise.scale
             uncertainty_scores += uncertainty_score(cov).tolist()
             for qry_id, uncertainty in zip(qry_ids, uncertainty_scores):
                 qpp_scores[qry_id]['uncertainty'] = uncertainty
@@ -83,4 +94,4 @@ def main(run_id: str, data_cfg: DatasetConfig):
 if __name__ == '__main__':
     args = OmegaConf.load('src/utils/config.yml')
     data_cfg = DatasetConfig(args.prepare_data.dataset_id)
-    main(args.wandb.run_id, data_cfg)
+    main(args.train, args.wandb.run_id, data_cfg)
