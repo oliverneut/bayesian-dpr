6a329df9385c3390e636086e0d95c1bc1a9236b3
oliverneut <oliver.neut@gmail.com>
2025-06-06 16:31:34 +0200

    Modified qpp

--- src/qpp.py
+++ src/qpp.py
@@ -8,75 +8,63 @@ from types import SimpleNamespace
 from indexing import FaissIndex
 from utils.model_utils import vbll_model_factory
 from pytrec_eval import RelevanceEvaluator
-from data_loaders import get_qrels
-from torch.utils.data import DataLoader
-from data_loaders import EmbeddingDataset
 from utils.data_utils import DatasetConfig
 from tqdm import tqdm
 from collections import defaultdict
 import logging
+import ir_datasets
 
 logger = logging.getLogger(__name__)
 
 def uncertainty_score(cov):
     return torch.linalg.vector_norm(cov, ord=2, dim=1)
 
-def main(args: SimpleNamespace, run_id: str, data_cfg: DatasetConfig):
-    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO)
-    logger.info(f"Run ID: {run_id}")
-    logger.info(f"Dataset id: {data_cfg.dataset_id}")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model_dir = f"output/models/{run_id}"
-    psg_embs_path = f"{model_dir}/psg_embs.pt"
-    psg_ids_path = f"{model_dir}/psg_ids.pt"
-    qry_embs_path = f"{model_dir}/qry_embs.pt"
-    qry_ids_path = f"{model_dir}/qry_ids.pt"
+def uncertainty_score(scale, unc_method="norm"):
+    cov = torch.diag(scale.squeeze())
 
-    psg_embs = torch.load(psg_embs_path)
-    psg_ids = torch.load(psg_ids_path)
+    if unc_method == "norm":
+        return torch.linalg.norm(cov)
+    elif unc_method == "trace":
+        return torch.trace(cov)
+    elif unc_method == "det":
+        _, logdet = torch.linalg.slogdet(cov)
+        return logdet
+    elif unc_method == "entropy":
+        d = cov.size(0)
+        _, logdet = torch.linalg.slogdet(cov)
+        return 0.5 * d * torch.log(torch.tensor(2 * torch.pi * torch.e)) + 0.5 * logdet
+    else:
+        raise ValueError(f"Unknown uncertainty method: {unc_method}")
 
-    model_path = f"{model_dir}/model.pt"
 
-    _, model = vbll_model_factory(args.model_name, 1, args.parameterization, args.prior_scale, args.wishart_scale, device)
-    model.load_state_dict(torch.load(model_path, map_location=device))
-    model.eval()
+def infer_embedding(model, tokenizer, query, device):
+    qry_enc = tokenizer(query, padding="max_length", truncation=True, max_length=32, return_tensors="pt").to(device)
+    qry_emb = model(qry_enc).predictive
 
-    noise_scale = model.vbll_layer.noise().scale.to(torch.device("cpu"))
+    return qry_emb
 
-    qry_dataset = EmbeddingDataset(qry_embs_path, qry_ids_path)
-    qry_dataloader = DataLoader(qry_dataset, batch_size=16, shuffle=False)
 
-    index = FaissIndex.build(psg_embs[:,0,:])
-
-    qrels = get_qrels(data_cfg.get_qrels_file(split=data_cfg.test_name))
-
-    uncertainty_scores = []
-    ndcg_scores = []
-    mrr_scores = []
-    run = {}
+def qpp(data, model, tokenizer, index, psg_ids, device, unc_method="norm"):
+    run = defaultdict(dict)
     qpp_scores = defaultdict(dict)
 
     with torch.no_grad():
-        for qry_embs, qry_ids in tqdm(qry_dataloader, desc="Computing QPP scores"):
-            mean, cov = qry_embs[:,0,:], qry_embs[:,1,:]
+        for qid, qry in tqdm(data.queries, desc="Computing QPP scores"):
+            emb = infer_embedding(model, tokenizer, qry, device)
 
-            # cov = cov - noise_scale
-            u_scores = uncertainty_score(cov).tolist()
-            uncertainty_scores += u_scores
-            for qry_id, uncertainty in zip(qry_ids, u_scores):
-                qpp_scores[qry_id]['uncertainty'] = uncertainty
+            uncertainty = uncertainty_score(emb.scale, unc_method).item()
 
-            scores, indices = index.search(mean, k=10)
+            qpp_scores[qid]['uncertainty'] = uncertainty
 
-            batch_psg_indices = [[psg_ids[idx] for idx in batch_indices] for batch_indices in indices]
-            
-            for qry_id, query_scores, query_indices in zip(qry_ids, scores, batch_psg_indices):
-                    run[qry_id] = {}
-                    for score, psg_id in zip(query_scores, query_indices):
-                        run[qry_id][psg_id] = float(score)
+            scores, indices = index.search(emb.loc, k=10)
 
-        evaluator = RelevanceEvaluator(qrels, {"ndcg", "recip_rank"})
+            psg_indices = [psg_ids[idx] for idx in indices[0]]
+        
+            for score, psg_id in zip(scores[0], psg_indices):
+                run[qid][psg_id] = float(score)
+        
+        evaluator = RelevanceEvaluator(data.qrels_dict(), {"ndcg", "recip_rank"})
         results = evaluator.evaluate(run)
 
         for qry_id, metrics in results.items():
@@ -95,7 +83,38 @@ def main(args: SimpleNamespace, run_id: str, data_cfg: DatasetConfig):
     logger.info(f"nDCG: {np.mean(ndcg_scores)}")
     logger.info(f"MRR: {np.mean(mrr_scores)}")
 
+
+def main(args: SimpleNamespace, run_id: str, unc_method="norm"):
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO)
+    logger.info(f"Run ID: {run_id}")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model_dir = f"output/models/{run_id}"
+    psg_embs = torch.load(f"{model_dir}/psg_embs.pt")
+    psg_ids = torch.load(f"{model_dir}/psg_ids.pt")
+
+    model_path = f"{model_dir}/model.pt"
+
+    tokenizer, model = vbll_model_factory(args.model_name, 1, args.parameterization, args.prior_scale, args.wishart_scale, device)
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
+
+    trec_dl_19 = ir_datasets.load("msmarco-passage/trec-dl-2019/judged")
+    trec_dl_20 = ir_datasets.load("msmarco-passage/trec-dl-2020/judged")
+
+    index = FaissIndex.build(psg_embs[:,0,:])
+    print(f'TREC DL 2019: {len(trec_dl_19)} queries')
+    qpp(trec_dl_19, model, tokenizer, index, psg_ids, device, unc_method=unc_method)
+    print('')
+
+    print(f'TREC DL 2019: {len(trec_dl_20)} queries')
+    qpp(trec_dl_20, model, tokenizer, index, psg_ids, device, unc_method=unc_method)
+
+
 if __name__ == '__main__':
     args = OmegaConf.load('config.yml')
     data_cfg = DatasetConfig(args.prepare_data.dataset_id)
-    main(args.train, args.wandb.run_id, data_cfg)
+    api = wandb.Api()
+    config = api.run(f"{args.wandb.entity}/{args.wandb.project}/{args.wandb.run_id}").config
+    params = SimpleNamespace(**config)
+    main(params, args.wandb.run_id, "norm")
