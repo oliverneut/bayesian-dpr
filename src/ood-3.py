2a7c2acd6e8a94247b53d09d828777733902315b
oliverneut <oliver.neut@gmail.com>
2025-06-12 18:19:49 +0200

    Fixed ood-3 experiment

---
+++ src/ood-3.py
@@ -0,0 +1,182 @@
+import sys
+import os
+from typing import Dict
+import json
+from tqdm import tqdm
+from pathlib import Path
+import numpy as np
+from omegaconf import OmegaConf
+import torch
+import logging
+import torch.nn.functional as F
+import wandb
+from indexing import FaissIndex
+from vbll.layers.regression import VBLLReturn
+from scipy.stats import pointbiserialr
+from sklearn.metrics import roc_auc_score, average_precision_score
+from utils.data_utils import DatasetConfig
+from data_loaders import get_queries, get_qrels
+from utils.model_utils import vbll_model_factory, model_factory
+
+
+logger = logging.getLogger(__name__)
+
+# base_path = os.path.abspath(os.path.join('../..'))
+# sys.path.append(base_path)
+
+
+def prepare_test_queries(test_queries: list, queries: Dict, data_cfg: DatasetConfig, num_samples: int, OOD: bool) -> None:
+    """Prepare test queries dataset."""
+    qrels = get_qrels(data_cfg.get_qrels_file(split=data_cfg.test_name))
+    
+    i = 0
+    for qid, rels in qrels.items():
+        if len(rels) > 0:
+            test_queries.append({"query": queries[qid], "OOD": OOD})
+            i += 1
+        
+        if i >= num_samples: break
+    
+    return test_queries
+
+
+def infer_query(qry: str, tokenizer,  model):
+    qry_enc = tokenizer(qry, padding="max_length", truncation=True, max_length=32, return_tensors="pt")
+    qry_emb = model(qry_enc)
+    return qry_emb
+
+
+def uncertainty_score(qry_emb, unc_method="norm"):
+    cov = qry_emb.covariance.squeeze()
+
+    if unc_method == "norm":
+        return torch.sqrt(qry_emb.trace_covariance)
+    elif unc_method == "trace":
+        return qry_emb.trace_covariance
+    elif unc_method == "det":
+        return qry_emb.logdet_covariance
+    elif unc_method == "entropy":
+        d = cov.size(0)
+        logdet = qry_emb.logdet_covariance
+        return 0.5 * d * torch.log(torch.tensor(2 * torch.pi * torch.e)) + 0.5 * logdet
+    else:
+        raise ValueError(f"Unknown uncertainty method: {unc_method}")
+
+
+def calculate_uncertainty_scores(data, tokenizer, model, unc_method="norm"):
+    uncertainty_scores = []
+    labels = []
+    for query_data in tqdm(data, desc="Calculating uncertainty scores"):       
+        emb = infer_query(query_data['query'], tokenizer, model)
+
+        uncertainty_scores.append(uncertainty_score(emb.predictive, unc_method).item())
+        labels.append(query_data['OOD'])
+
+    return np.array(uncertainty_scores), np.array(labels)
+
+
+def metrics(uncertainty_scores, labels):
+    auc = roc_auc_score(labels, uncertainty_scores)
+    print(f"AUROC: {auc}")
+    aupr = average_precision_score(labels, uncertainty_scores)
+    print(f"AUPR: {aupr}")
+    pbs = pointbiserialr(labels, uncertainty_scores)
+    print(f"Point Biserial Correlation: {pbs.correlation}, p-value: {pbs.pvalue}")
+
+
+def calculate_baseline_scores(queries, tokenizer,  model, index, T=50):
+    
+    msp_scores = []
+    entropy_scores = []
+    energy_scores = []
+    labels = []
+
+    for query_data in tqdm(queries, desc="Calculating baseline scores"):
+        qry_emb = infer_query(query_data['query'], tokenizer, model)
+        labels.append(query_data['OOD'])
+    
+        if isinstance(qry_emb, VBLLReturn):
+            qry_emb = qry_emb.predictive.loc
+        
+        scores, _ = index.search(qry_emb, k=10)
+        scores = torch.from_numpy(scores[0])
+        shifted_scores = scores - scores.max()
+        scaled_scores = shifted_scores / T
+        probs = torch.softmax(scaled_scores, dim=0)
+
+        msp_score = torch.max(probs).item()
+        entropy = -torch.sum(probs * torch.log(probs + 1e-10)).item()
+        energy = -torch.log(torch.sum(torch.exp(scaled_scores))).item()
+
+        msp_scores.append(msp_score)
+        entropy_scores.append(entropy)
+        energy_scores.append(energy)
+
+    return np.array(msp_scores), np.array(entropy_scores), np.array(energy_scores), np.array(labels)
+
+
+def main(model_name: str, vbll: bool, run_id: str, T=50):
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO)
+    logger.info(f"Run ID: {run_id}")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger.info(f"Using device: {device}")
+
+    save_dir = f"output/models/{run_id}"
+    model_path = f"{save_dir}/model.pt"
+
+    if vbll:
+        tokenizer, model = vbll_model_factory(model_name, device)
+    else:
+        tokenizer, model = model_factory(model_name, device)
+
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
+
+    if os.path.exists(f"{save_dir}/psg_embs.pt") and os.path.exists(f"{save_dir}/psg_ids.pt"):
+        logger.info("Loading precomputed embeddings and IDs from disk.")
+        psg_embs = torch.load(f"{save_dir}/psg_embs.pt", map_location=device)
+        psg_ids = torch.load(f"{save_dir}/psg_ids.pt")
+
+        if psg_embs.dim() == 3:
+            logger.info("Reshaping embeddings from 3D to 2D.")
+            psg_embs = psg_embs[:,0]
+    else:
+        logger.info("No precomputed embeddings found. Please run the encoding script first.")
+        return
+    
+    index = FaissIndex.build(psg_embs)
+
+    msmarco_cfg = DatasetConfig('msmarco')
+    msmarco_queries = get_queries(msmarco_cfg.get_queries_file())
+
+    for ood_dataset in ['nq', 'hotpotqa', 'fiqa']:
+        ood_cfg = DatasetConfig(ood_dataset)
+        ood_queries = get_queries(ood_cfg.get_queries_file())
+
+        msmarco_ood_queries = []
+        msmarco_ood_queries = prepare_test_queries(msmarco_ood_queries, msmarco_queries, msmarco_cfg, 1000, OOD=False)
+        msmarco_ood_queries = prepare_test_queries(msmarco_ood_queries, ood_queries, ood_cfg, 1000, OOD=True)
+        
+        unc_method = "norm"
+        if vbll:
+            uncertainty_scores, labels = calculate_uncertainty_scores(msmarco_ood_queries, tokenizer, model, unc_method="norm")
+            print(f"Uncertainty scores calculated using method {unc_method}")
+            metrics(uncertainty_scores, labels)
+            print('')
+
+        msp_scores, entropy_scores, energy_scores, labels = calculate_baseline_scores(msmarco_ood_queries, tokenizer, model, index, T)
+        print(f"Baseline scores calculated")
+        metrics(msp_scores, labels)
+        metrics(entropy_scores, labels)
+        metrics(energy_scores, labels)
+
+
+if __name__ == '__main__':
+    args = OmegaConf.load('config.yml')
+    api = wandb.Api()
+    config = api.run(f"{args.wandb.entity}/{args.wandb.project}/{args.wandb.run_id}").config
+    model_id = config['model_name']
+    vbll = config['knowledge_distillation']
+    main(model_id, vbll, args.wandb.run_id, T=50)
+    
