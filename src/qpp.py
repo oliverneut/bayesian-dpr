00f02878181940be2dbbd89e931f1704d720d89b
oliverneut <oliver.neut@gmail.com>
2025-06-17 21:55:32 +0200

    Fixed qpp

--- src/qpp.py
+++ src/qpp.py
@@ -5,7 +5,11 @@ import wandb
 import numpy as np
 from omegaconf import OmegaConf
 from utils.indexing import FaissIndex
-from utils.model_utils import vbll_model_factory
+from utils.model_utils import get_model_from_run
+from utils.run_utils import RunConfig
+from utils.embedding_utils import has_embeddings, load_embeddings
+from utils.data_utils import DatasetConfig
+from torch.utils.data import Dataset
 from pytrec_eval import RelevanceEvaluator
 from collections import defaultdict
 import logging
@@ -200,184 +204,8 @@ def load_json(file_path: str, file_name: str):
         return json.load(f)
 
 
-class PredictorCalculator:
-    def __init__(self, data, dataset_name):
-        self.qpp_scores = defaultdict(dict)
-        self.scores = defaultdict(dict)
-        self.data = data
-        self.dataset_name = dataset_name
-        self.model = None
-        self.tokenizer = None
-        self.psg_embs = None
-        self.index_reader = LuceneIndexReader.from_prebuilt_index('msmarco-v1-passage')
-        self.query = self.get_query_dict()
-        self.qtoken_set = self.get_qtoken_set()
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-
-    def set_model(self, model_dir, model_name):
-        model_path = f"{model_dir}/model.pt"
-        self.tokenizer, self.model = vbll_model_factory(model_name, self.device)
-
-        self.model.load_state_dict(torch.load(model_path, map_location=self.device))
-        self.model.eval()
-
-
-    def calculate_uncertainty_scores(self, model_name, run_id):
-        model_dir = f"output/models/{run_id}"
-        self.set_model(model_dir, model_name)
-        psg_embs = torch.load(f"{model_dir}/psg_embs.pt")
-        psg_ids = torch.load(f"{model_dir}/psg_ids.pt")
-
-        index = FaissIndex.build(psg_embs[:,0,:])
-        run = defaultdict(dict)
-
-        with torch.no_grad():
-            for qry_id, qry in self.data.queries:
-                qry_enc = self.tokenizer(qry, padding="max_length", truncation=True, max_length=32, return_tensors="pt").to(self.device)
-                qry_emb = self.model(qry_enc).predictive
-
-                for unc_method in UNC_METHODS:
-                    self.qpp_scores[qry_id][unc_method] = uncertainty_score(qry_emb, unc_method).item()
-
-                scores, indices = index.search(qry_emb.loc, k=10)
-                psg_indices = [psg_ids[idx] for idx in indices[0]]
-                for score, psg_id in zip(scores[0], psg_indices):
-                    run[qry_id][psg_id] = float(score)
-            
-            evaluator = RelevanceEvaluator(self.data.qrels_dict(), {"ndcg", "recip_rank"})
-            results = evaluator.evaluate(run)
-
-            for qry_id, metrics in results.items():
-                self.scores[qry_id]['ndcg'] = metrics['ndcg']
-                self.scores[qry_id]['mrr'] = metrics['recip_rank']
-
-            ndcg_scores = [self.scores[qry_id]['ndcg'] for qry_id in self.scores]
-            mrr_scores = [self.scores[qry_id]['mrr'] for qry_id in self.scores]
-            logger.info(f"nDCG: {np.mean(ndcg_scores)}")
-            logger.info(f"MRR: {np.mean(mrr_scores)}")
-
-
-    def calculate_baseline_scores(self):
-        qtoken2var, qtoken2std, qtoken2did = self.get_token_stats()
-
-        pred_performance = {}
-
-        for qry_id, qry in tqdm(self.query.items(), total=len(self.query)):
-            if qry_id not in self.data.qrels_dict():
-                continue
-
-            pred_performance[qry_id] = {}
-            qtokens = self.index_reader.analyze(qry)
-
-            self.qpp_scores[qry_id]["QS"] = QS(qtokens, self.index_reader, qtoken2did)
-
-            pmi_avg, pmi_max, pmi_sum = avg_max_sum_PMI(qtokens, self.index_reader, qtoken2did)
-            self.qpp_scores[qry_id]["PMI_avg"] = pmi_avg
-            self.qpp_scores[qry_id]["PMI_max"] = pmi_max
-            self.qpp_scores[qry_id]["PMI_sum"] = pmi_sum
-
-            self.qpp_scores[qry_id]["ql"] = len(qtokens)
-
-            var_avg, var_max, var_sum = avg_max_sum_VAR(qtokens, qtoken2var)
-            self.qpp_scores[qry_id]["var_avg"] = var_avg
-            self.qpp_scores[qry_id]["var_max"] = var_max
-            self.qpp_scores[qry_id]["var_sum"] = var_sum
-
-            std_avg, std_max, std_sum = avg_max_sum_VAR(qtokens, qtoken2std)
-            self.qpp_scores[qry_id]["std_avg"] = std_avg
-            self.qpp_scores[qry_id]["std_max"] = std_max
-            self.qpp_scores[qry_id]["std_sum"] = std_sum
-
-            idf_avg, idf_max, idf_sum, idf_std = avg_max_sum_std_IDF(qtokens, self.index_reader)
-            self.qpp_scores[qry_id]["idf_avg"] = idf_avg
-            self.qpp_scores[qry_id]["idf_max"] = idf_max
-            self.qpp_scores[qry_id]["idf_sum"] = idf_sum
-
-            scq_avg, scq_max, scq_sum = avg_max_sum_SCQ(qtokens, self.index_reader)
-            self.qpp_scores[qry_id]["scq_avg"] = scq_avg
-            self.qpp_scores[qry_id]["scq_max"] = scq_max
-            self.qpp_scores[qry_id]["scq_sum"] = scq_sum
-
-    
-    def get_query_dict(self):
-        query = {}
-        for qry_id, qry in self.data.queries_iter():
-            query[qry_id] = qry
-        return query
-    
-
-    def get_qtoken_set(self):
-        qtoken_set = set()
-
-        for _, qry in self.data.queries_iter():
-            qtokens = self.index_reader.analyze(qry)
-            for qtoken in qtokens:
-                qtoken_set.add(qtoken)
-        
-        return qtoken_set
-    
-
-    def token_stats_exists(self):
-        qtoken2var = os.path.exists(f"output/qpp/{self.dataset_name}/qtoken2var.json")
-        qtoken2std = os.path.exists(f"output/qpp/{self.dataset_name}/qtoken2std.json")
-        qtoken2did = os.path.exists(f"output/qpp/{self.dataset_name}/qtoken2did.json")
-        return qtoken2var and qtoken2std and qtoken2did
-    
-
-    def load_token_stats(self):
-        qtoken2var = load_json(f"output/qpp/{self.dataset_name}", "qtoken2var.json")
-        qtoken2std = load_json(f"output/qpp/{self.dataset_name}", "qtoken2std.json")
-        qtoken2did = load_json(f"output/qpp/{self.dataset_name}", "qtoken2did.json")
-        return qtoken2var, qtoken2std, qtoken2did
-    
-
-    def save_token_stats(self, qtoken2var, qtoken2std, qtoken2did):
-        if not os.path.exists(f"output/qpp/{self.dataset_name}"):
-            os.makedirs(f"output/qpp/{self.dataset_name}")
-
-        with open(f"output/qpp/{self.dataset_name}/qtoken2var.json", 'w', encoding='utf-8') as f:
-            json.dump(qtoken2var, f, ensure_ascii=False, indent=4)
-
-        with open(f"output/qpp/{self.dataset_name}/qtoken2std.json", 'w', encoding='utf-8') as f:
-            json.dump(qtoken2std, f, ensure_ascii=False, indent=4)
-
-        with open(f"output/qpp/{self.dataset_name}/qtoken2did.json", 'w', encoding='utf-8') as f:
-            json.dump(qtoken2did, f, ensure_ascii=False, indent=4)
-
-
-    def get_token_stats(self):
-        if self.token_stats_exists():
-            logger.info("Loading precomputed token stats.")
-            return self.load_token_stats()
-
-        qtoken2var, qtoken2std, qtoken2did = {}, {}, {}
-
-        for qtoken in tqdm(self.qtoken_set):
-            qtoken2var[qtoken], qtoken2std[qtoken] = VAR(qtoken, self.index_reader)
-            qtoken2did[qtoken] = t2did(qtoken, self.index_reader)
-
-        self.save_token_stats(qtoken2var, qtoken2std, qtoken2did)
-
-        return qtoken2var, qtoken2std, qtoken2did
-
-
-def calculate_qpp_scores(data, dataset_name, model_name: str, run_id: str, device: torch.device):
-    predictor_calc = PredictorCalculator(data, dataset_name)
-
-    predictor_calc.calculate_uncertainty_scores(model_name, run_id)
-    
-    if False:
-        predictor_calc.calculate_baseline_scores()
-
-    return predictor_calc.scores, predictor_calc.qpp_scores
-
-
-def calculate_correlations(scores, qpp_scores):
+def calculate_correlations(ndcg_scores, mrr_scores, qpp_scores):
     qpp_scores_predictors = qpp_scores[list(qpp_scores.keys())[0]].keys()
-
-    ndcg_scores = [scores[qry_id]['ndcg'] for qry_id in scores]
-    mrr_scores = [scores[qry_id]['mrr'] for qry_id in scores]
 
     for predictor in qpp_scores_predictors:
         predictor_scores = [qpp_scores[qry_id][predictor] for qry_id in qpp_scores]
@@ -394,29 +222,183 @@ def calculate_correlations(scores, qpp_scores):
         logger.info('------------------------------------------------------')
 
 
-def main(model_name: str, run_id: str):
-    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO)
-    logger.info(f"Run ID: {run_id}")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def get_query_dict(data):
+    query = {}
+    for qry_id, qry in data.queries_iter():
+        query[qry_id] = qry
+    return query
+
+
+def get_qtoken_set(data, index_reader):
+    qtoken_set = set()
+
+    for _, qry in data.queries_iter():
+        qtokens = index_reader.analyze(qry)
+        for qtoken in qtokens:
+            qtoken_set.add(qtoken)
     
-    dataset_name = "trec-dl-2019"
-    trec_dl_19 = ir_datasets.load(f"msmarco-passage/{dataset_name}/judged")
-    scores_19, qpp_scores_19 = calculate_qpp_scores(trec_dl_19, dataset_name, model_name, run_id, device)
+    return qtoken_set
 
-    logger.info(f"Scores for {dataset_name}")
-    calculate_correlations(scores_19, qpp_scores_19)
-    logger.info('')
 
-    dataset_name = "trec-dl-2020"
-    trec_dl_20 = ir_datasets.load(f"msmarco-passage/{dataset_name}/judged")
-    scores_20, qpp_scores_20 = calculate_qpp_scores(trec_dl_20, dataset_name, model_name, run_id, device)
+def token_stats_exists(dataset_name: str):
+    qtoken2var = os.path.exists(f"output/qpp/{dataset_name}/qtoken2var.json")
+    qtoken2std = os.path.exists(f"output/qpp/{dataset_name}/qtoken2std.json")
+    qtoken2did = os.path.exists(f"output/qpp/{dataset_name}/qtoken2did.json")
+    return qtoken2var and qtoken2std and qtoken2did
 
-    logger.info(f"Scores for {dataset_name}")
-    calculate_correlations(scores_20, qpp_scores_20)
+
+def load_token_stats(dataset_name: str):
+    qtoken2var = load_json(f"output/qpp/{dataset_name}", "qtoken2var.json")
+    qtoken2std = load_json(f"output/qpp/{dataset_name}", "qtoken2std.json")
+    qtoken2did = load_json(f"output/qpp/{dataset_name}", "qtoken2did.json")
+    return qtoken2var, qtoken2std, qtoken2did
+
+
+def save_token_stats(qtoken2var, qtoken2std, qtoken2did, dataset_name: str):
+    if not os.path.exists(f"output/qpp/{dataset_name}"):
+        os.makedirs(f"output/qpp/{dataset_name}")
+
+    with open(f"output/qpp/{dataset_name}/qtoken2var.json", 'w', encoding='utf-8') as f:
+        json.dump(qtoken2var, f, ensure_ascii=False, indent=4)
+
+    with open(f"output/qpp/{dataset_name}/qtoken2std.json", 'w', encoding='utf-8') as f:
+        json.dump(qtoken2std, f, ensure_ascii=False, indent=4)
+
+    with open(f"output/qpp/{dataset_name}/qtoken2did.json", 'w', encoding='utf-8') as f:
+        json.dump(qtoken2did, f, ensure_ascii=False, indent=4)
+
+
+def get_token_stats(data, index_reader: LuceneIndexReader, dataset_name: str):
+    qtoken2var, qtoken2std, qtoken2did = {}, {}, {}
+    
+    qtoken_set = get_qtoken_set(data, index_reader)
+
+    for qtoken in tqdm(qtoken_set):
+        qtoken2var[qtoken], qtoken2std[qtoken] = VAR(qtoken, index_reader)
+        qtoken2did[qtoken] = t2did(qtoken, index_reader)
+
+    save_token_stats(qtoken2var, qtoken2std, qtoken2did, dataset_name)
+
+    return qtoken2var, qtoken2std, qtoken2did
+
+
+def calculate_baseline_scores(qpp_scores: dict, data: Dataset, dataset_name):
+    index_reader = LuceneIndexReader.from_prebuilt_index('msmarco-v1-passage')
+
+    if token_stats_exists(dataset_name):
+        logger.info("Loading precomputed token stats.")
+        qtoken2var, qtoken2std, qtoken2did = load_token_stats(dataset_name)
+    else:
+        logger.info("Calculating token stats.")
+        qtoken2var, qtoken2std, qtoken2did = get_token_stats(data, index_reader, dataset_name)
+
+    pred_performance = {}
+
+    query_dict = get_query_dict(data)
+
+    for qry_id, qry in tqdm(query_dict.items(), total=len(query_dict)):
+        if qry_id not in data.qrels_dict():
+            continue
+        
+        pred_performance[qry_id] = {}
+        qtokens = index_reader.analyze(qry)
+
+        qpp_scores[qry_id]["QS"] = QS(qtokens, index_reader, qtoken2did)
+
+        pmi_avg, pmi_max, pmi_sum = avg_max_sum_PMI(qtokens, index_reader, qtoken2did)
+        qpp_scores[qry_id]["PMI_avg"] = pmi_avg
+        qpp_scores[qry_id]["PMI_max"] = pmi_max
+        qpp_scores[qry_id]["PMI_sum"] = pmi_sum
+
+        qpp_scores[qry_id]["ql"] = len(qtokens)
+
+        var_avg, var_max, var_sum = avg_max_sum_VAR(qtokens, qtoken2var)
+        qpp_scores[qry_id]["var_avg"] = var_avg
+        qpp_scores[qry_id]["var_max"] = var_max
+        qpp_scores[qry_id]["var_sum"] = var_sum
+
+        std_avg, std_max, std_sum = avg_max_sum_VAR(qtokens, qtoken2std)
+        qpp_scores[qry_id]["std_avg"] = std_avg
+        qpp_scores[qry_id]["std_max"] = std_max
+        qpp_scores[qry_id]["std_sum"] = std_sum
+
+        idf_avg, idf_max, idf_sum, idf_std = avg_max_sum_std_IDF(qtokens, index_reader)
+        qpp_scores[qry_id]["idf_avg"] = idf_avg
+        qpp_scores[qry_id]["idf_max"] = idf_max
+        qpp_scores[qry_id]["idf_sum"] = idf_sum
+
+        scq_avg, scq_max, scq_sum = avg_max_sum_SCQ(qtokens, index_reader)
+        qpp_scores[qry_id]["scq_avg"] = scq_avg
+        qpp_scores[qry_id]["scq_max"] = scq_max
+        qpp_scores[qry_id]["scq_sum"] = scq_sum
+
+    return qpp_scores
+
+
+def calculate_uncertainty_scores(data, tokenizer, model, index, psg_ids, device):
+    qpp_scores = defaultdict(dict)
+    run = defaultdict(dict)
+
+    with torch.no_grad():
+        for qry_id, qry in data.queries:
+            qry_enc = tokenizer(qry, padding="max_length", truncation=True, max_length=32, return_tensors="pt").to(device)
+            qry_emb = model(qry_enc).predictive
+
+            for unc_method in UNC_METHODS:
+                qpp_scores[qry_id][unc_method] = uncertainty_score(qry_emb, unc_method).item()
+
+            scores, indices = index.search(qry_emb.loc, k=10)
+            psg_indices = [psg_ids[idx] for idx in indices[0]]
+            for score, psg_id in zip(scores[0], psg_indices):
+                run[qry_id][psg_id] = float(score)
+        
+        evaluator = RelevanceEvaluator(data.qrels_dict(), {"ndcg", "recip_rank"})
+        results = evaluator.evaluate(run)
+
+        for qry_id, metrics in results.items():
+            qpp_scores[qry_id]['ndcg'] = metrics['ndcg']
+            qpp_scores[qry_id]['mrr'] = metrics['recip_rank']
+
+        ndcg_scores = [qpp_scores[qry_id]['ndcg'] for qry_id in qpp_scores]
+        mrr_scores = [qpp_scores[qry_id]['mrr'] for qry_id in qpp_scores]
+        logger.info(f"nDCG: {np.mean(ndcg_scores)}")
+        logger.info(f"MRR: {np.mean(mrr_scores)}")
+    
+    return ndcg_scores, mrr_scores, qpp_scores
+
+
+def main(run_cfg: RunConfig, embs_dir: str):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger.info(f"Using device: {device}")
+
+    tokenizer, model = get_model_from_run(run_cfg, device)
+
+    data_cfg = DatasetConfig('msmarco')
+
+    if has_embeddings(run_cfg, data_cfg, embs_dir):
+        psg_embs, psg_ids = load_embeddings(run_cfg, data_cfg, embs_dir, rel_mode="dpr", device=device)
+    else:
+        logger.info("No precomputed embeddings found. Please run the eval_retriever script first.")
+        return
+
+    index = FaissIndex.build(psg_embs)
+
+    for dataset_name in ["trec-dl-2019", "trec-dl-2020"]:
+        trec_dl = ir_datasets.load(f"msmarco-passage/{dataset_name}/judged")
+        ndcg_scores, mrr_scores, qpp_scores = calculate_uncertainty_scores(trec_dl, tokenizer, model, index, psg_ids, device)
+        qpp_scores = defaultdict(dict)
+        qpp_scores = calculate_baseline_scores(qpp_scores, trec_dl, dataset_name)
+
+        logger.info(f"Calculating correlations for {dataset_name}")
+        calculate_correlations(ndcg_scores, mrr_scores, qpp_scores)
+        logger.info(f"Finished processing {dataset_name}")
 
 
 if __name__ == '__main__':
+    logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO)
+    logger.info("QPP experiment")
     args = OmegaConf.load('config.yml')
-    api = wandb.Api()
-    config = api.run(f"{args.wandb.entity}/{args.wandb.project}/{args.wandb.run_id}").config
-    main(config['model_name'], args.wandb.run_id)
+
+    run_cfg = RunConfig(args)
+
+    main(run_cfg, args.eval.embs_dir)
